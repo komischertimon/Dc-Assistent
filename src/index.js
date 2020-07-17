20330c9252192c8089a5d21b2725b509a1eb78d1
Sebastian Ulrich <sesi.ulrich@gmail.com>
2020-07-17 11:29:00 +0200

    may error handling for timeout

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 const Discord = require('discord.js');
-const fs = require("fs");
+const fs = require('fs');
 const { dirname } = require('path');
 
 const config = JSON.parse(fs.readFileSync(`${__dirname}/config.json`,'utf-8'));
-const package = JSON.parse(fs.readFileSync(`${__dirname}/package.json`,'utf-8'));
-const save = JSON.parse(fs.readFileSync(`${__dirname}/save.json`, "utf-8"));
 
 const comAlliasses = JSON.parse(fs.readFileSync(`${__dirname}/shortcuts.json`, "utf-8"));
 
@@ -46,6 +44,10 @@ notifier.on('ready', ()=>{
     setTimeout(setup, 8000, client, client2, notifier);
 });
 
+notifier.on('messageReactionAdd', (reaction, user)=>{
+    let run = require(`${__dirname}/automatic/msgNotReactHandle.js`);
+    run(client, client2, notifier, reaction, user);
+});
 client.on('voiceStateUpdate', (oldMember, newMember)=>{    
     if(oldMember.id == client.user.id || oldMember.id == client2.user.id){
         let run = require(`${__dirname}/automatic/voiceCallActivity.js`);
@@ -54,86 +56,79 @@ client.on('voiceStateUpdate', (oldMember, newMember)=>{
 });
 
 client.on('message', (msg)=>{
+    var save = JSON.parse(fs.readFileSync(`${__dirname}/save.json`, "utf-8"));
     if(save.afk){
         let awhas = require(`${__dirname}/automatic/absenceAssistants.js`);
         awhas(client, client2, notifier, msg, "1");
     }
 });
 client2.on('message', (msg)=>{
+    var save = JSON.parse(fs.readFileSync(`${__dirname}/save.json`, "utf-8"));
     if(save.afk){
         let awhas = require(`${__dirname}/automatic/absenceAssistants.js`);
-        awhas(msg, "2");
+        awhas(client, client2, notifier, msg, "2");
     }
 });
-notifier.on('message', async (msg)=>{
-    var content = msg.content.split(' ');
+notifier.on('message', (msg)=>{
+    var save = JSON.parse(fs.readFileSync(`${__dirname}/save.json`, "utf-8")),
+    content = msg.content.split(' ');
 
-    if(msg.channel.id == config.confchannel){
-        if(content[0].startsWith(config.prefix)){
-            content[0] = content[0].substr(config.prefixlengh);
-            
-            if(comAlliasses[content[0]]){content[0] = comAlliasses[content[0]]}
-            try{
-                let command = require(`${__dirname}/notCommands/${content[0]}.js`);
-                command(client, client2, notifier, msg, content);
-            }catch(err){
-                console.log(err);
-            }
-        }else{
-            if(comAlliasses[content[0]]){content[0] = comAlliasses[content[0]]}
-            try{
-                let command = require(`${__dirname}/commands/${content[0]}.js`);
-                command(client, client2, notifier, msg, content);
-            }catch(err){
-                console.log(err);
+    if(msg.author.id != notifier.user.id){
+        if(msg.channel.id == config.confchannel){
+            if(content[0].startsWith(config.prefix)){
+                content[0] = content[0].substr(config.prefixlengh);
+                if(comAlliasses[content[0]]){content[0] = comAlliasses[content[0]]}
+
+                try{
+                    let command = require(`${__dirname}/notCommands/${content[0]}.js`);
+                    command(client, client2, notifier, msg, content);
+                }catch(err){
+                    if(err.code == 'MODULE_NOT_FOUND'){
+                        msg.channel.send("command not found.");
+                    }else{
+                        console.log(err);
+                    }
+                }
+            }else{
+                if(comAlliasses[content[0]]){content[0] = comAlliasses[content[0]]}
+
+                try{
+                    let command = require(`${__dirname}/commands/${content[0]}.js`);
+                    command(client, client2, notifier, msg, content);
+                }catch(err){
+                    if(err.code == 'MODULE_NOT_FOUND'){
+                        msg.channel.send("command not found.");
+                    }else{
+                        console.log(err);
+                    }
+                }
             }
         }
     }
 
-    if(msg.channel.type == "dm" && reply){
-        if(msg.author.id == client.user.id || msg.author.id == client2.user.id){
+    if(msg.channel.type == "dm" && save.reply){
+        console.log("isset");
+        if(msg.author.id == client2.user.id){
+            console.log("answer sent");
             client.users.get(save.reply).send(msg.content);
             save.reply = false;
+
+            fs.writeFileSync(`${__dirname}/save.json`, JSON.stringify(save));
         }
     }
 });
 
-notifier.on('messageReactionAdd', (reaction, user)=>{
-    let run = require(`${__dirname}/automatic/msgNotReactHandle.js`);
-    run(reaction, user);
+client.on("error", (err)=>{
+    console.log(err);
+});
+client2.on("error", (err)=>{
+    console.log(err);
+});
+notifier.on("error", (err)=>{
+    console.log(err);
 });
 
-
-
-
 //client Logins
-try{
-    client.login(config.token)
-    client2.login(config.token2)
-    notifier.login(config.tokennotifier)
-}catch(err){
-    console.log(err);
-    if(err.code == 'EAI_AGAIN' || err.code == 'ETIMEDOUT' || err.code == 'ERR_UNHANDLED_ERROR'){
-        console.log(`------------------------\n>CONNECTION ERROR DETECTED<\n------------------------`);
-        setTimeout(function(){
-            destroyAll();
-            client.login(config.token)
-            client2.login(config.token2)
-            notifier.login(config.tokennotifier)
-        }, 30000);
-    }else{
-        console.log(err);
-    }
-}
-//weird error handle
-function destroyAll(){
-    try{
-        client.destroy();
-    }catch(e){};
-    try{
-        client2.destroy();
-    }catch(e){};
-    try{
-        notifier.destroy();
-    }catch(e){};
-}
+client.login(config.token);
+client2.login(config.token2);
+notifier.login(config.tokennotifier);
