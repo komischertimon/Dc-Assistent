f57c8468a517c2846f6a6d77b09862375ee2f9c6
Sebastian Ulrich <sesi.ulrich@gmail.com>
2020-07-20 20:29:44 +0200

    aw concept created

--- src/automatic/automaticWriting.js
+++ src/automatic/automaticWriting.js
@@ -1,3 +1,13 @@
 module.exports = (client, client2, notifier, msg, content) => {
-    
+    var stor = {
+        salutations: new Array("hi", "hallo", "servus", "moin", "guten tag"),
+        goodbyes: new Array("tschüss", "bye", "tschö")
+    };
+
+    if(stor.salutations.includes(msg.content)){
+        msg.channel.send(`Moin <@${msg.author.id}>`);
+    }
+    else if(stor.goodbyes.includes(msg.content)){
+        msg.channel.send(`hau rein <@${msg.author.id}>`);
+    }
 }
